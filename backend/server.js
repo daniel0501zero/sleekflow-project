e4c1fc4b7ebdc707bd4b60548630de18ff67dd33
daniel0501zero <daniel0501zero@gmail.com>
2026-01-30 19:08:05 +0800

    Revert to original version

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ import NotFound from './middleware/notFound.js'
 import cookieParser from 'cookie-parser';
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: ["http://localhost:5173", "http://localhost:3000"],
     credentials: true
 }));
 app.use(express.json());
@@ -20,21 +20,26 @@ app.use(cookieParser());
 
 // Debug middleware
 app.use((req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
+  console.log(`${req.method} ${req.url}`);
+  next();
 });
 
 app.use('/api/v1/users', authRouter);
 app.use('/api/v1/todos', router);
 app.use(NotFound)
 
+
 const start = async () => {
-  try {
-    await connectDB(url);
-    app.listen(port, () => console.log(`Server running on port ${port}`));
-  } catch (err) {
-    console.log(err);
-  }
-};
+    try {
+        await connectDB(url);
+        app.listen(port, () => 
+            console.log(`app listening at port ${port}`)
+        )
+    }
+    catch (err) {
+        console.log(err);
+    }
+
+}
 
 start();
